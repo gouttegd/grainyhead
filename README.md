f516cd0fddfe1373195ad3453be1aeda62b4a93e
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2021-07-27 18:20:42 +0100

    Initial commit.
    This is GrainyHead, a set of Python tools to work with GitHub
    repositories from the command line.

---
+++ README.md
@@ -0,0 +1,12 @@
+GrainyHead - Helper tools for GitHub
+====================================
+
+GrainyHead is a set of Python scripts to work with GitHub repositories
+from the command line.
+
+
+Copying
+-------
+GrainyHead is distributed under the terms of the GNU General Public
+License, version 3 or higher. The full license is included in the
+[COPYING file](COPYING) of the source distribution.
---
+++ setup.py
@@ -0,0 +1,42 @@
+# grainyhead - Helper tools for GitHub
+# Copyright (C) 2021 Damien Goutte-Gattat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptool import setup
+from fbcam.grainyhead import __version__
+
+setup(
+    name='grainyhead',
+    version=__version__,
+    description='Helper tools for GitHub',
+    author='Damien Goutte-Gattat',
+    author_email='dpg44@cam.ac.uk',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Licence :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.9'
+        ],
+
+    install_requires=[
+        'click',
+        'ghapi'
+        ],
+
+    packages=[
+        'fbcam',
+        'fbcam.grainyhead'
+        ]
+    )
